8130e5b2192d9fcbf1a86f0d782fb505adf9ca0b
qwerty123-lab <mstas8278@gmail.com>
2024-12-12 18:15:51 +0300

    converting data to common values

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from config import open_weather_token
+import datetime
 import requests
-from pprint import *
+import pprint
 
 def get_weather(city, open_weather_token):
     try:
         r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={open_weather_token}"
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
             )
         data = r.json()
-        print(data)
+        #pprint(data)
+
+        city=data["name"]
+        cur_weather=data["main"]["temp"]
+        humidity=data["main"]["humidity"]
+        pressure=data["main"]["pressure"]
+        wind=data["wind"]["speed"]
+        sunrise_timestamp=datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp=datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+
+        print(f"Погода в городе: {city}\nТемпература: {cur_weather} C\n"
+              f"Влажность: {humidity} %\nДавление: {pressure} мм.рт.ст\n"
+              f"Ветер: {wind} м/c\nРассвет: {sunrise_timestamp}\nЗакат: {sunset_timestamp}")
+
     except Exception as ex:
         print(ex)
         print("Проверьте название города")
