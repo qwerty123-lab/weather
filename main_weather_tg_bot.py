a3ebd5c3ecd08fb3a18f27148740e49e4eec2c44
qwerty123-lab <mstas8278@gmail.com>
2024-12-12 19:22:19 +0300

    adding the get_weather function, editing the library code, launching the bot

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -12,6 +12,33 @@ dp=Dispatcher(bot)
 async def start_command(message: types.Message):
     await message.reply("Привет! напиши мне название города и я пришлю сводку погоды!")
 
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+            )
+        data = r.json()
+        #pprint(data)
+
+        city=data["name"]
+        cur_weather=data["main"]["temp"]
+        humidity=data["main"]["humidity"]
+        pressure=data["main"]["pressure"]
+        wind=data["wind"]["speed"]
+        sunrise_timestamp=datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp=datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day=datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%d-%m-%Y %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather} °C\n"
+              f"Влажность: {humidity} %\nДавление: {pressure} мм.рт.ст\n"
+              f"Ветер: {wind} м/c\nРассвет: {sunrise_timestamp}\nЗакат: {sunset_timestamp}\n"
+              f"Световой день: {length_of_the_day}"
+              )
+
+    except:
+        await message.reply("Проверьте название города")
 
 
 if __name__=="__main__":
